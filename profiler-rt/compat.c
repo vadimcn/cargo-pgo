bc1082ee70b7862fec3f0a61880fb8f57652fdb9
Vadim Chugunov <vadimcn@gmail.com>
2016-09-08 18:21:29 -0700

    Register runtime in __llvm_profile_override_default_filename

--- profiler-rt/compat.c
+++ profiler-rt/compat.c
@@ -1,5 +1,9 @@
 void __llvm_profile_set_filename(const char *Name);
+void __llvm_profile_register_write_file_atexit();
+void __llvm_profile_initialize_file();
 
 void __llvm_profile_override_default_filename(const char *Name) {
+    __llvm_profile_register_write_file_atexit();
     __llvm_profile_set_filename(Name);
+    __llvm_profile_initialize_file();
 }
